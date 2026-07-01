bf4fe0b96282f88d7ca8249e1812bf0c44713a45
TiagoDev <tiago23.mec@gmail.com>
2026-06-30 22:14:58 -0300

    factor: mover dados simulados para  diretorio dedicado'

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,22 @@
+import type	{ Customer } from '../types.ts';
+
+export const customers: Customer[] = [
+	{
+        id: 1,
+		name: 'Cristiano Ronaldo',
+        email: 'cristiano.ronaldo@example.com',
+		status: true,
+	},
+	{
+        id: 2,
+		name: 'Lionel Messi',
+        email: 'lionel.messi@example.com',
+		status: true,
+	},
+	{
+        id: 3,
+		name: 'Neymar Júnior',
+        email: 'Neymar.junior@gmail.com',
+		status: false,
+	},
+];
