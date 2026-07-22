e04c086bdcaf547fb2cc9d48255c29c25030ec7e
TiagoDev <tiago23.mec@gmail.com>
2026-07-21 22:27:52 -0300

    ajustando erro

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,15 +2,15 @@ import type { Request, Response, NextFunction } from 'express';
 import { NotFoundError } from '../errors/index.ts';
 
 export default function errorHandler(
-    error: unknown,
-    _request: Request,
-    response: Response ,
-    next: NextFunction) 	{
-        if (error instanceof NotFoundError) {
-            response.status(error.statusCode).json({ error: error.message });
-            return;
-    
-     }
-      console.log(error);
-      response.status(500).json	({ message: 'Erro interno do servidor.' });
-    }
+	error: unknown,
+	_request: Request,
+	response: Response,
+	_next: NextFunction,
+) {
+	if (error instanceof NotFoundError) {
+		response.status(error.statusCode).json({ error: error.message });
+		return;
+	}
+	console.log(error);
+	response.status(500).json({ message: 'Erro interno do servidor.' });
+}
