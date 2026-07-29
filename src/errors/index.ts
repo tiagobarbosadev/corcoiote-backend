f38241f5477c86574de80828e2c389222c1d4648
TiagoDev <tiago23.mec@gmail.com>
2026-07-28 22:18:14 -0300

    feat: adicionar captura de erro para ma requisicao

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -8,3 +8,12 @@
 
     }
 }
+
+export class ValidationError extends Error {
+    statusCode: number;
+
+    constructor(message: string) {
+        super(message);
+        this.statusCode = 400;
+    }
+}
